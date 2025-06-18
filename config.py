366f6fb298fa349bf699ee139a6177a1387e35c2
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-06-18 07:44:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/Silenthrax/SHRUTI")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/Silenthrax/BIGFM")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
